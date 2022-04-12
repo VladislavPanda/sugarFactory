fa75fca4ecebe4f42f83016fa6d381c46e2957eb
VladislavS999 <phpmailer1999@gmail.com>
2022-04-12 22:19:10 +0300

    установлена админка для менеджера

---
+++ database/migrations/2022_04_11_143232_create_packs_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('packs', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('good_id')->nullable();
+            $table->text('photo')->nullable();
+            $table->string('weight', 30)->nullable();
+            $table->string('forma')->nullable();
+            $table->text('group')->nullable();
+
+            $table->foreign('good_id')->on('goods')->references('id');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('packs');
+    }
+};
